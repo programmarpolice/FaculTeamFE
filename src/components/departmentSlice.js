b853ba627f3b187af927b44209722775abd3264c
Blake <bpatin77@gmail.com>
2024-10-22 15:28:32 -0500

    Created Slice files

---
+++ src/components/departmentSlice.js
@@ -0,0 +1,48 @@
+import api from "./api";
+const departmentApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getDepartments: build.query({
+      query: () => "departments",
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["department"],
+    }),
+    getDepartment: build.query({
+      query: (id) => "departments/" + id,
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["department"],
+    }),
+    updateDepartment: build.mutation({
+      query: (department) => ({
+        url: "departments",
+        method: "PUT",
+        body: department, //body links to the req.body of the department.js file
+      }),
+      invalidatesTags: ["department"],
+    }),
+    addDepartment: build.mutation({
+      query: (department) => ({
+        url: "departments",
+        method: "POST",
+        body: department,
+      }),
+      invalidatesTags: ["department"],
+    }),
+    deleteDepartment: build.mutation({
+      query: (id) => ({
+        url: "departments/" + id,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["department"],
+    }),
+  }),
+});
+// When exporting queries, you put the words "use" and "query"
+// around whatever name you chose above when injecting endpoints
+export const {
+  useGetDepartmentsQuery,
+  useGetDepartmentQuery,
+  useAddDepartmentMutation,
+  useDeleteDepartmentMutation,
+} = departmentApi;
---
+++ src/components/professorSlice.js
@@ -0,0 +1,48 @@
+import api from "./api";
+const professorApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getProfessors: build.query({
+      query: () => "professors",
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["professor"],
+    }),
+    getProfessor: build.query({
+      query: (id) => "professors/" + id,
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["professor"],
+    }),
+    updateProfessor: build.mutation({
+      query: (professor) => ({
+        url: "professors",
+        method: "PUT",
+        body: professor,
+      }),
+      invalidatesTags: ["professor"],
+    }),
+    addProfessor: build.mutation({
+      query: (professor) => ({
+        url: "professors",
+        method: "POST",
+        body: professor,
+      }),
+      invalidatesTags: ["professor"],
+    }),
+    deleteProfessor: build.mutation({
+      query: (id) => ({
+        url: "professors/" + id,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["professor"],
+    }),
+  }),
+});
+// When exporting queries, you put the words "use" and "query"
+// around whatever name you chose above when injecting endpoints
+export const {
+  useGetProfessorsQuery,
+  useGetProfessorQuery,
+  useAddProfessorMutation,
+  useDeleteProfessorMutation,
+} = professorApi;
