6d7d586c80076a08b4fc668e76188391686f4315
Blake <bpatin77@gmail.com>
2024-10-21 20:45:51 -0500

    Created and coded both js and css files for the footer. Additionally set up a potential layout for the App.js file

---
+++ App.js
@@ -0,0 +1,28 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Footer from "./Footer";
+// import About from "./About";
+// import Help from "./Help"; // Create Help component
+// import Support from "./Support"; // Create Support component
+// import FAQs from "./FAQs"; // Create FAQs component
+// import SocialMedia from "./SocialMedia"; // Create Social Media component
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Switch>
+//           <Route path="/about" component={About} />
+//           <Route path="/help" component={Help} />
+//           <Route path="/support" component={Support} />
+//           <Route path="/faqs" component={FAQs} />
+//           <Route path="/social-media" component={SocialMedia} />
+//           {/* Other routes */}
+//         </Switch>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
