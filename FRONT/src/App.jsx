b60935379e0987bf4dfd712e98b1f5f1b205e280
Matthew De Andrade <fayrfax@gmail.com>
2024-10-21 21:55:58 -0400

    added CSS for NavBar, and added Body Component.

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
-
+import { useEffect } from "react";
 import "./App.css";
+import { NavBar } from "./components/NavBar.jsx";
+import { Body } from "./components/Body.jsx";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [depts, setDepts] = useState([]);
+  const [faculty, setFaculty] = useState([]);
   return (
     <>
       <NavBar />
-
-      <Footer />
+      <Body login={login} setLogin={setLogin} />
     </>
   );
 }
